b13e7d54ef16225c9634760e710e1a7363bf8826
MarioRibeiro <mario.ribeiro.biomed@gmail.com>
2024-09-14 10:57:46 +0100

    Set up basic Flask backend and React frontend

---
+++ core/frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Welcome to CORE Fitness Studio</h1>
+      </header>
+      <main>
+        <p>Building a stronger you!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
